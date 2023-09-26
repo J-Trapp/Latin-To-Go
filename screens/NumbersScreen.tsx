3e7e968c6898070479d3c008dcc3d184f7a2e241
Jessica Trapp <jessica.maria.trapp@gmail.com>
2023-09-26 09:43:11 +0200

    Added card from react-native-paper and designed the NumbersScreen with cardText, input field and check-button

--- screens/NumbersScreen.tsx
+++ screens/NumbersScreen.tsx
@@ -1,21 +1,75 @@
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { RootStackParamList } from "../App";
+import { Button, Card, TextInput } from "react-native-paper";
 
-type Props = NativeStackScreenProps<RootStackParamList, "Numbers">;
+export default function NumbersScreen() {
+  const [userAnswer, setUserAnswer] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const correctAnswer = "unus";
 
-export default function NumbersScreen(props: Props) {
+  const checkAnswer = () => {
+    if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+      setFeedback("Correct!");
+    } else {
+      setFeedback("Wrong!");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Numbers</Text>
+      <Card style={styles.card}>
+        <Card.Content>
+          <Text style={styles.cardText}>one</Text>
+        </Card.Content>
+      </Card>
+      <TextInput
+        style={styles.input}
+        label="Enter Latin translation"
+        onChangeText={(text) => setUserAnswer(text)}
+        value={userAnswer}
+      />
+      <Button mode="contained" style={styles.checkButton} onPress={checkAnswer}>
+        Check
+      </Button>
+      <Text style={styles.feedback}>{feedback}</Text>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e0d1ff",
+  },
+  card: {
+    width: 200,
+    height: 250,
+    marginBottom: 20,
+    borderRadius: 10,
+    elevation: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardText: {
+    fontSize: 24,
+    fontFamily: "serif",
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingVertical: 20,
+  },
+
+  input: {
+    width: 200,
+    marginTop: 16,
+    marginBottom: 16,
+  },
+  checkButton: {
+    marginTop: 16,
+  },
+  feedback: {
+    fontSize: 18,
+    marginTop: 16,
   },
 });
